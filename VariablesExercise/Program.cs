bc07b03fbe7ac41e6624629afd01518b92adcccb
Ruben Burgos <code.rburgos@gmail.com>
2022-10-13 23:16:46 -0500

    What have I done...

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,50 @@
-﻿namespace VariablesExercise
+﻿using System.Runtime.Intrinsics.Arm;
+
+namespace VariablesExercise
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string hisName = "Shrek";
+
+            int hisHeight = 8;
+
+            char ogreOnion = 'O';
+
+            bool hollywoodStar = true;
+
+            double ticketCost = 5.66d;
+
+            decimal lifetimeGross = 267665011.00m;
+
+            // ⡴⠑⡄⠀⠀⠀⠀⠀⠀⠀⣀⣀⣤⣤⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+            // ⠸⡇⠀⠿⡀⠀⠀⠀⣀⡴⢿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀
+            // ⠀⠀⠀⠀⠑⢄⣠⠾⠁⣀⣄⡈⠙⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀
+            // ⠀⠀⠀⠀⢀⡀⠁⠀⠀⠈⠙⠛⠂⠈⣿⣿⣿⣿⣿⠿⡿⢿⣆⠀⠀⠀⠀⠀⠀
+            // ⠀⠀⠀⢀⡾⣁⣀⠀⠴⠂⠙⣗⡀⠀⢻⣿⣿⠭⢤⣴⣦⣤⣹⠀⠀⠀⢀⢴⣶
+            // ⠀⠀⢀⣾⣿⣿⣿⣷⣮⣽⣾⣿⣥⣴⣿⣿⡿⢂⠔⢚⡿⢿⣿⣦⣴⣾⠁⠸⣼
+            // ⠀⢀⡞⠁⠙⠻⠿⠟⠉⠀⠛⢹⣿⣿⣿⣿⣿⣌⢤⣼⣿⣾⣿⡟⠉⠀⠀⠀⠀
+            // ⠀⣾⣷⣶⠇⠀⠀⣤⣄⣀⡀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀
+            // ⠀⠉⠈⠉⠀⠀⢦⡈⢻⣿⣿⣿⣶⣶⣶⣶⣤⣽⡹⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀
+            // ⠀⠀⠀⠀⠀⠀⠀⠉⠲⣽⡻⢿⣿⣿⣿⣿⣿⣿⣷⣜⣿⣿⣿⡇⠀⠀⠀⠀⠀
+            // ⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣷⣶⣮⣭⣽⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀
+            // ⠀⠀⠀⠀⠀⠀⣀⣀⣈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀
+            // ⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀
+            // ⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀
+            // ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠻⠿⠿⠿⠿⠛⠉
+            // Shrek is life.
+
+            Console.WriteLine("Here are some swamptastic " + hisName + " facts that you didn't know you needed.");
+            Console.WriteLine(hisName + " is " + hisHeight + " feet tall! He ain't no Lord Farquaad!");
+            Console.WriteLine(ogreOnion + " is for " + hisName +"'s famous line from the movie.");
+            Console.WriteLine("Ogres are like onions. Onions have layers. Ogres have layers.");
+            Console.WriteLine("Did you know Shrek has a Hollywood Star? It's " + hollywoodStar + "!");
+            Console.WriteLine("I remeber when I was a young boy and my Mother took me to see " + hisName + " in the movie theater.");
+            Console.WriteLine("Tickets only cost $" + ticketCost + " back in 2001!");
+            Console.WriteLine("Speaking of ticket cost, our sweet ogre boy made a lifetime gross of $" + lifetimeGross + "!");
+            Console.WriteLine("Well, I hope you enjoyed these swamptastic " + hisName + " facts!");
+
         }
     }
 }
